55382de3e2bbf183231a7373433fdf20080e4dce
Ansh <ansh.shetty.22@gmail.com>
2025-10-27 18:49:48 +0530

    Added Show Password functionaity. Middleware fixes left for onboarding redirection issue

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -6,12 +6,14 @@ import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import { createClient } from "@/lib/supabase/client";
 import { useRouter } from "next/navigation";
+import { EyeOff, Eye } from "lucide-react";
 
 const oldStandard = Old_Standard_TT({ subsets: ["latin"], weight: "400" });
 
 export default function Signin() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setshowPassword] = useState(false);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const supabase = createClient();
@@ -94,8 +96,25 @@ export default function Signin() {
               <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="name@example.com" className="w-full h-[30px] rounded-[5px] bg-[#F4F4F4] border border-[#0D1717] [border-width:0.5px] px-3 text-[12px] leading-[1.3em] text-[#0D1717] placeholder:text-[#959595]" />
 
               <p className="font-sans text-[12px] leading-[1.3em] text-[#0D1717] mt-[12px] mb-[4px]">Password</p>
-              <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="password" className="w-full h-[30px] rounded-[5px] bg-[#F4F4F4] border border-[#0D1717] [border-width:0.5px] px-3 text-[12px] leading-[1.3em] text-[#0D1717] placeholder:text-[#959595]" />
-              
+              <div className="relative w-full">
+                <Input
+                  type={showPassword ? "text" : "password"}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="password"
+                  className="w-full h-[30px] rounded-[5px] bg-[#F4F4F4] border border-[#0D1717] 
+               [border-width:0.5px] px-3 pr-10 text-[12px] leading-[1.3em] 
+               text-[#0D1717] placeholder:text-[#959595]"
+                />
+
+                <button
+                  type="button"
+                  onClick={() => setshowPassword(!showPassword)}
+                  className="absolute right-2 top-[50%] -translate-y-1/2 text-[#0D1717]"
+                >
+                  {showPassword ? <EyeOff size={16} /> : <Eye size={16} />}
+                </button>
+              </div>
               <div className="w-full flex justify-end mt-[8px]">
                 <a href="/forgot-password" className="font-sans text-[12px] leading-[1.3em] text-[#1DC6A1] hover:underline">
                   Forgot password?
